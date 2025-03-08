cc39da5421428c7900838f5f1e87045829839ba6
Hari Krishna <hari.100.hks@outlook.com>
2025-03-08 11:51:03 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,23 @@
 import os
 import google.generativeai as genai
+import deepnote_toolkit
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+deepnote_toolkit.set_integration_env()
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+thinking_model = genai.GenerativeModel("gemini-2.0-flash-thinking-exp")
+lite = genai.GenerativeModel("gemini-2.0-flash-lite")
 model = genai.GenerativeModel("gemini-2.0-flash")
 
-def get_gemini_response(prompt):
+
+def get_gemini_response(prompt, type):
     try:
-        response = model.generate_content(prompt)
+        if type == "thinking":
+            response = thinking_model.generate_content(prompt)
+        elif type == "lite":
+            response = lite.generate_content(prompt)
+        else:
+            response = model.generate_content(prompt)
         return response.text.strip()
     except Exception as e:
         return f"Error: {e}"
